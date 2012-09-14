e2e4036198a1cdb21fa023e73bfc3a1be727e71a
Bevan Stephens <bevan@bevanstephens.com>
2012-09-14 13:14:22 +0100

    Improved collapsable TOC js.

--- assets/js/fontforge.js
+++ assets/js/fontforge.js
@@ -3,14 +3,14 @@
 $(".table-of-contents").collapse();
 
 $(".table-of-contents").on('shown', function () {
-  	$('.toc-toggle span').fadeOut(function() {
-  		$('.toc-toggle span').text("Hide all guides").fadeIn('fast');
+  	$(this).find(".toc-toggle span").fadeOut(function() {
+  		$(this).text("Hide all guides").fadeIn('fast');
 	});
 })
 
 $(".table-of-contents").on('hidden', function () {
-  $('.toc-toggle span').fadeOut(function() {
-  		$('.toc-toggle span').text("Show all guides").fadeIn('fast');
+  $(this).find(".toc-toggle span").fadeOut(function() {
+  		$(this).text("Show all guides").fadeIn('fast');
 	});
 })
 
