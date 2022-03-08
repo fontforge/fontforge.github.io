54e9768df8bb8995fe37a0db0eee075b0c53a3d8
Jeremy Tan <jtanx@outlook.com>
2022-03-08 22:26:48 +1100

    Update documentation

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '20201107',
+    VERSION: '20220308',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
